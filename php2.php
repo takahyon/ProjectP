06ae5eeea7990d3ad8b1966ec8f026f027899e9e
Takamasa Iijima <takahyon@gmail.com>
2018-04-13 14:46:50 +0900

    [add]hello.php and php2.php

---
+++ php2.php
@@ -0,0 +1,36 @@
+<?php
+
+$header = ["name"=>'名前',"address"=>'住所', "phone"=>'電話', "Email"=>'email'];
+$addresses = array(
+    1=>array("name"=>'東京太郎',"address"=>'東京都', "phone"=>'012-345-6789', "Email"=>'taro@example.com'),
+    2=>array("name"=>'工科花子',"address"=>'北海道', "phone"=>'987-654-3210', "Email"=>'hana@example.com'),
+);
+
+?>
+
+<table>
+  <table border="1">
+    <tr>
+        <th>名前</th><th>住所</th><th>電話</th><th>Email</th>
+    </tr>
+
+
+<?php
+
+foreach($addresses as $v){
+
+        echo "<tr>\n";
+        echo "<td>".$v['name']."</td>\n";
+        echo "<td>".$v['address']."</td>\n";
+        echo "<td>".$v['phone']."</td>\n";
+        echo "<td>".$v['Email']."</td>\n";
+        echo "</tr>\n";
+
+}
+
+?>
+</table>
+
+    <html>
+    <
+    </html>
