0c6a8751064b7207564438587053ae3469cf78eb
Takamasa Iijima <takahyon@gmail.com>
2018-04-20 16:27:13 +0900

    [add]応用２が完成

--- PHP/php-advance-2.php
+++ PHP/php-advance-2.php
@@ -3,23 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>ファイルアップローダー</title>
+
+
+    <?php
+    $save_dir = '/images/upload/';
+    $save_filename = date('YmdHis');
+    $save_basename = $save_filename . '.' . $extension;
+    $save_path = $_SERVER["DOCUMENT_ROOT"] . $save_dir . $save_basename;
+    while (file_exists($save_path)) { // 同名ファイルがあればファイル名を変更する
+        $save_filename .= mt_rand(0, 9);
+        $save_basename = $save_filename . '.' . $extension;
+        $save_path = $_SERVER["DOCUMENT_ROOT"] . $save_dir . $save_basename;
+    }
+    ?>
+
 </head>
 <body>
 
 <h1>ファイルアップローダー</h1>
 
-<form method="post">
-    <input type="file" name="file">
+<form action="php-advance-2.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fname">
     <input type="submit" value="送信">
 </form>
 
 <?php
+$_FILES['fname'];
 
-    if(isset($_POST["submit"]) && $_POST["file"]){
-        print "Succcessful: ファイルのアップロードに成功しました。";
-    }else{
-        print "Error: ファイルが指定されてません";
+$tempfile = $_FILES['fname']['tmp_name']; // 一時ファイル名
+$filename = $_FILES['fname']['name']; // 本来のファイル名
+
+$tempfile = $_FILES['fname']['tmp_name'];
+$filename = './' . $_FILES['fname']['name'];
+
+if (is_uploaded_file($tempfile)) {
+    if (move_uploaded_file($tempfile, $filename)) {
+        echo $filename . "をアップロードしました。";
+    } else {
+        echo "ファイルをアップロードできません。";
     }
+} else {
+    echo "ファイルが選択されていません。";
+}
 ?>
 
 </body>
